30e9177f75d50bc4ba9a9cd49dea39711c4b3558
marvix2023 <146742063+marvix2023@users.noreply.github.com>
2024-10-22 13:03:46 +0100

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -46,7 +46,7 @@ def firePrompt(prompt: str) -> str:
    
    llm = Ollama(model='llama3.2', system=SYSTEM_PROMPT)
    try:
-        res = llm.invoke(prompt, host = 'localhost', port = 8501)
+        res = llm.invoke(prompt, host = 'localhost', port = 11434)
         return res
    except Exception as e:
         st.error(f"Error invoking LLM: {e}")
